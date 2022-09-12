28bfbf1eb219c1defc9d4811a1c54134faa8facf
Richard Qiu <>
2022-09-12 00:27:26 -0400

    fix container bug

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar"
 import ContactMe from "./contact"
-import { Container } from '@mui/material';
+// import { Container } from '@mui/material';
 import Intro from "./aboutIntro"
 import Projects from "./projects"
 
