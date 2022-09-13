1c033e22a52651f6f3a4c2d0d3b4994d0dd25055
Richard Qiu <qiur12@mcmaster.ca>
2022-09-13 12:37:30 -0400

    test again

--- client/src/pages/projects.jsx
+++ client/src/pages/projects.jsx
@@ -2,7 +2,7 @@ function Project () {
   return (
     <section id="contacts">
       <h1 style={{ textAlign: "center" }}>My project portfolio</h1>
-      <h2>sdads</h2>
+      <h2></h2>
     </section>
   )
 }
