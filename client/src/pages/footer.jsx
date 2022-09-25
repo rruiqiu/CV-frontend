cacf0637fa69f514d787e36bc79847d3acb9538f
Richard Qiu <qiur12@mcmaster.ca>
2022-09-25 02:12:35 -0400

    add return to home route button

--- client/src/pages/footer.jsx
+++ client/src/pages/footer.jsx
@@ -5,10 +5,11 @@ function footer () {
   return (
     <footer id="footer">
       <div className="links">
+        <a className="footericonposition" href="/"><Icon icon="ant-design:home-outlined" color="white" width="25" /></a>
         <a className="footericonposition" href="https://github.com/rruiqiu"><FaGithub color={"white"} size={"24px"} /></a>
         <a className="footericonposition" href="https://www.linkedin.com/in/rruiqiu/"><FaLinkedin color={"white"} size={"24px"} /></a>
         <a className="footericonposition" href="https://github.com/rruiqiu"><Icon icon="pepicons:cv" color="white" width="24" height="24" /></a>
-        <a className="footericonposition" href="/"><Icon icon="ant-design:mail-outlined" color="white" width="26" height="26" /></a>
+
       </div>
 
       <div className="footerparagraph">
