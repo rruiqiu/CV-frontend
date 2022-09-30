577039f50ea2de1f8a671c1d3887342a58be75d6
Richard Qiu <qiur12@mcmaster.ca>
2022-09-30 02:34:54 -0400

    update some features

--- client/src/pages/footer.jsx
+++ client/src/pages/footer.jsx
@@ -9,7 +9,6 @@ function footer () {
         <a className="footericonposition" href="https://github.com/rruiqiu" target="_blank" rel="noopener noreferrer"><FaGithub size={"24px"} /></a>
         <a className="footericonposition" href="https://www.linkedin.com/in/rruiqiu/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={"24px"} /></a>
         <a className="footericonposition" href="https://drive.google.com/file/d/1o7KKdE5aJQRItD9JuWg49isheq3yBJzH/view?usp=sharing" target="_blank" rel="noopener noreferrer"><Icon icon="pepicons:cv" width="24" height="24" /></a>
-
       </div>
 
       <div className="footerparagraph">
--- client/src/projectlisting.jsx
+++ client/src/projectlisting.jsx
@@ -1,18 +1,27 @@
 const projectlisting = [
   {
     id: 1,
-    img: "/images/tofsensor.png",
-    name: "SPATIAL-MAPPING",
-    description: "Designed and Built an embedded spatial measurement system using a time-of-flight sensor to acquire in-formation about the area surrounded clientsDeveloped a Python Program to map spatial information into 3D by using NumPy and Open3D librariesSuccessful completion of software and hardware communication through UART and I2CImplement precise control of the 28BYJ-48 stepper motor to assist the data acquisition of the ToF sensor Significantly reduced the cost of purchasing Commercial Light Detection and Ranging(LIDAR) equipment",
-    stack: ["Embedded C", "Python", "MCU", "I2C/UART"],
+    img: "/images/portfolio.png",
+    name: "Personal-Portfolio",
+    description: "Designed and built an online personal portfolio from scratch using React. Constructed a REST API backend for the contacts management frontend page. With authentication login and page router protection and more...",
+    stack: ["React", "MongoDB", "Node.js", "Express"],
     github: "https://github.com/rruiqiu/SPATIAL-MAPPING-USING-TIME-OF-FLIGHT",
     demo: "https://ruiqiu.netlify.app/",
   },
   {
     id: 2,
+    img: "/images/tofsensor.png",
+    name: "SPATIAL-MAPPING",
+    description: "Designed and Built an embedded spatial measurement system using a time-of-flight sensor to acquire in-formation about the area surrounded clients. Using Python NumPy and Open3D to map spatial information into 3D. UART and I2C protocol for Hardware and Software communication.",
+    stack: ["Embedded C", "Python", "MCU", "I2C/UART"],
+    github: "https://github.com/rruiqiu/SPATIAL-MAPPING-USING-TIME-OF-FLIGHT",
+    // demo: "https://ruiqiu.netlify.app/",
+  },
+  {
+    id: 3,
     img: "/images/plant.png",
     name: "McMaster Recycling Plant Sorting System",
-    description: "In a group of three, by collaborating and communicating, designed and developed an efficient Python programme for the McMaster Recycling Plant Sorting System, by successfully running sorting simulation to distinguish seven different types plastics through the IR(infrared) techniques, the sorting accuracy has significantly increased from 50% to 93%",
+    description: "In a small group of three, designed and developed an efficient Python program for the McMaster Recycling Plant Sorting System, by using the IR(infrared) techniques, significantly increased the sorting accuracy from 50% to 93%",
     stack: ["Python", "Pandas"],
     github: "https://github.com/rruiqiu/SPATIAL-MAPPING-USING-TIME-OF-FLIGHT",
   }
